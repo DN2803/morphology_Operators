b0794311673d7aa350323835d7ded4bc5a73b950
dn28203 <ntkngann2k3@gmail.com>
2024-04-20 16:15:52 +0700

    add menu type of morphology

--- function/UI.py
+++ function/UI.py
@@ -45,6 +45,8 @@ class ImageProcessorApp:
         self.import_type_label.grid(row=2, column=0, padx=5, pady=5)
         self.import_type_menu = tk.OptionMenu(master, self.import_type_var, "Grayscale", "Binary")
         self.import_type_menu.grid(row=2, column=1, padx=5, pady=5)
+        
+        self.import_type_var.trace_add('write', self.update_process_options)  # Thiết lập phương thức theo dõi
 
         self.select_button = tk.Button(master, text="Select Image", command=self.open_image)
         self.select_button.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
@@ -61,13 +63,17 @@ class ImageProcessorApp:
         self.struct_entry.insert(tk.END, "1\n1\n1")
         self.struct_entry.grid(row=6, column=0, columnspan=2, padx=5, pady=5)
 
-        self.operation_var = tk.StringVar(master)
-        self.operation_var.set("Dilation")  # Default operation
+        self.process_options = ["Dilation", "Erosion", "Opening", "Closing", "Reconstruction"]  
+     
+        self.process_option_var = tk.StringVar(master)
+        self.process_option_var.set(self.process_options[0])  # Default operation
         self.operation_label = tk.Label(master, text="Select Operation:")
         self.operation_label.grid(row=7, column=0, columnspan=2, padx=5, pady=5)
-        self.operation_menu = tk.OptionMenu(master, self.operation_var, "Dilation", "Erosion")
-        self.operation_menu.grid(row=8, column=0, columnspan=2, padx=5, pady=5)
-
+        self.process_option_menu = tk.OptionMenu(master, self.process_option_var, *self.process_options)
+        self.process_option_menu.grid(row=8, column=0, columnspan=2, padx=5, pady=5)  
+        default_value = self.import_type_var.get()
+        if default_value:
+            self.update_process_options()
         self.process_button = tk.Button(master, text="Process Image", command=self.process_image)
         self.process_button.grid(row=9, column=0, columnspan=2, padx=5, pady=5)
 
@@ -82,7 +88,73 @@ class ImageProcessorApp:
             else:
                 self.image = grayscale_to_binary(file_path, 127)
             self.display_original_image()
+    def update_process_options(self, *args):
+        if self.import_type_var.get() == "Grayscale":
+            if "Top Hat" not in self.process_options:
+                self.process_options.append("Top Hat")
+            if "Smoothing" not in self.process_options: 
+                self.process_options.append("Smoothing")
+            if "Gradient" not in self.process_options: 
+                self.process_options.append("Gradient")
+            if "Textual segmentation" not in self.process_options: 
+                self.process_options.append("Textual segmentation")
+            if "Granulometry" not in self.process_options: 
+                self.process_options.append("Granulometry")
+            
 
+            if "Hit or Miss" in self.process_options:
+                self.process_options.remove("Hit or Miss")
+            if "Boundary Extraction" in self.process_options: 
+                self.process_options.remove("Boundary Extraction")
+            if "Region Filling" in self.process_options: 
+                self.process_options.remove("Region Filling")
+            if "Extraction of Connected Components" in self.process_options: 
+                self.process_options.remove("Extraction of Connected Components")
+            if "Convex Hull" in self.process_options: 
+                self.process_options.remove("Convex Hull")
+            if "Thinning" in self.process_options: 
+                self.process_options.remove("Thinning")
+            if "Thickening" in self.process_options: 
+                self.process_options.remove("Thickening")
+            if "Skeleton" in self.process_options: 
+                self.process_options.remove("Skeleton")  
+            if "Pruning" in self.process_options: 
+                self.process_options.remove("Pruning")    
+        else:
+            if "Top Hat" in self.process_options:
+                self.process_options.remove("Top Hat")
+            if "Smoothing" in self.process_options: 
+                self.process_options.remove("Smoothing")
+            if "Gradient" in self.process_options: 
+                self.process_options.remove("Gradient")
+            if "Textual segmentation" in self.process_options: 
+                self.process_options.remove("Textual segmentation")
+            if "Granulometry" in self.process_options: 
+                self.process_options.remove("Granulometry")
+           
+
+            if "Hit or Miss" not in self.process_options:
+                self.process_options.append("Hit or Miss")
+            if "Boundary Extraction" not in self.process_options: 
+                self.process_options.append("Boundary Extraction")
+            if "Region Filling" not in self.process_options: 
+                self.process_options.append("Region Filling")
+            if "Extraction of Connected Components" not in self.process_options: 
+                self.process_options.append("Extraction of Connected Components")
+            if "Convex Hull" not in self.process_options: 
+                self.process_options.append("Convex Hull")
+            if "Thinning" not in self.process_options: 
+                self.process_options.append("Thinning")
+            if "Thickening" not in self.process_options: 
+                self.process_options.append("Thickening")
+            if "Skeleton" not in self.process_options: 
+                self.process_options.append("Skeleton")  
+            if "Pruning" not in self.process_options: 
+                self.process_options.append("Pruning")    
+        # Update the process option menu
+        self.process_option_menu.destroy()  # Hủy bỏ menu cũ
+        self.process_option_menu = tk.OptionMenu(self.master, self.process_option_var, *self.process_options)  # Tạo menu mới với các tùy chọn đã cập nhật
+        self.process_option_menu.grid(row=8, column=0, columnspan=2, padx=5, pady=5)  # Sử dụng .grid() để đặt menu trong lưới
     def display_original_image(self):
         if self.image:
             self.image.thumbnail((300, 300))  # Resize the image to fit the window
@@ -112,11 +184,11 @@ class ImageProcessorApp:
         if self.import_type_var.get() == "Binary": 
             if not self.use_library_var.get():
                 print ("tu viet")
-                self.processed_image = binary_non_lib.binary_morphology(src_image, struct_array, self.operation_var.get())
+                self.processed_image = binary_non_lib.binary_morphology(src_image, struct_array, self.process_option_var.get())
                 self.processed_image = Image.fromarray(self.processed_image.astype(np.uint8)* 255)
             else: 
                 
-                self.processed_image = binary_lib.binary_morphology(src_image, struct_array, self.operation_var.get())
+                self.processed_image = binary_lib.binary_morphology(src_image, struct_array, self.process_option_var.get())
                 print(self.processed_image)
             # Convert the processed NumPy array to a PIL Image object
                 self.processed_image = Image.fromarray(self.processed_image.astype(np.uint8))
@@ -125,10 +197,10 @@ class ImageProcessorApp:
             if self.use_library_var.get():
                 print ("tu viet")
                 
-                self.processed_image = gray_non_lib.grayscale_morphology(src_image, struct_array, self.operation_var.get())
+                self.processed_image = gray_non_lib.grayscale_morphology(src_image, struct_array, self.process_option_var.get())
                 print(self.processed_image)
             else:
-                self.processed_image = gray_lib.grayscale_morphology(src_image, struct_array, self.operation_var.get())
+                self.processed_image = gray_lib.grayscale_morphology(src_image, struct_array, self.process_option_var.get())
             self.processed_image = Image.fromarray(self.processed_image.astype(np.uint8))
             self.display_processed_image()
 
