d9ca8733a8c49a88777b0adf4022caeff0a354cd
dn28203 <ntkngann2k3@gmail.com>
2024-04-24 23:25:04 +0700

    add openning and closing for grayscale

--- function/grayscale/non_lib/header_non_lib.py
+++ function/grayscale/non_lib/header_non_lib.py
@@ -1,5 +1,7 @@
 from . import dilation
 from . import erosion
+from . import opening
+from . import closing
 
 def grayscale_morphology(image, structuring_element, typeofOperator = "Dilation"):
     result = []
@@ -7,4 +9,8 @@ def grayscale_morphology(image, structuring_element, typeofOperator = "Dilation"
         return dilation.dilation(image, structuring_element = structuring_element)
     elif (typeofOperator == "Erosion"): 
         return erosion.erosion(image, structuring_element=structuring_element)
+    elif (typeofOperator == "Opening"):
+        return opening.opening(image, structuring_element = structuring_element)
+    elif (typeofOperator == "Closing"):
+        return closing.closing(image, structuring_element = structuring_element)
     return result
