239de0cce7c44e23d22db357ccd8823a9f60088f
dn28203 <ntkngann2k3@gmail.com>
2024-07-08 18:01:13 +0700

    update hit or miss

--- function/binary/non_lib/hit_or_miss.py
+++ function/binary/non_lib/hit_or_miss.py
@@ -1,20 +1,21 @@
-from . import erosion
+from scipy.ndimage import binary_erosion
 import numpy as np
 def hit_or_miss (image, structuring_element):
     # Tạo các bản sao của kernel
     b1 = structuring_element.copy()
-    b2 = structuring_element.copy()
-    print(image)
     
 
-    # Thay đổi giá trị 1 thành 0 và 0 thành 1 trong bản sao b2 của kernel
-    b2[b2==1] = 0
-    b2[b2==0] = 1
+  # Make a copy of the kernel
+    b2 = structuring_element.copy()
+
+    # Swap 1s and 0s in the copy
+    b2 = 1 - b2
+    print (b2)
 
     # Thực hiện phép xói mòn với kernel b1, b2 trên ảnh đầu vào
-    a = erosion.erosion(image,b1)
-    b = erosion.erosion(~(image), b2)
+    a =  binary_erosion(image,b1)
+    b =  binary_erosion(~image, b2)
     
     # Lấy phép giao giữa 2 ảnh đã xói mòn
-    hitmiss_img = np.logical_and(a,b)
+    hitmiss_img = a&b
     return hitmiss_img
