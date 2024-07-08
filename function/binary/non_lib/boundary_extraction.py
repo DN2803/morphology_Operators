fb8e1fa0aec238eb622dbc4ac68111175d575cd3
dn28203 <ntkngann2k3@gmail.com>
2024-07-08 18:18:15 +0700

    update

--- function/binary/non_lib/boundary_extraction.py
+++ function/binary/non_lib/boundary_extraction.py
@@ -1,4 +1,4 @@
 from . import erosion
 def boundary_extraction (image, structuring_element):
     erode = erosion.erosion(image, structuring_element)  #co đối tượng lại
-    return image - erode
+    return image/255 - erode
